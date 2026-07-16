a651e2a67686713ceb3dea99f0a758c105708c0d
Utkarsh Pandey <1exoticutkarsh@gmail.com>
2026-07-16 16:06:26 +0530

    Completed Spring Core Hands-on 2

--- spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,4 +1,9 @@
 package com.cognizant.springlearn;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringLearnApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("Application Started");
 
@@ -14,6 +19,20 @@ public class SpringLearnApplication {
 
         System.out.println("Application Running");
 
+        displayDate();
+    }
+
+    public static void displayDate() throws Exception {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        System.out.println(date);
     }
 
 }
